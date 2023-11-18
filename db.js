81b75836248294d29f9b3fdf7709bcb135e012a4
EsmaNErdem <esmaerdem94@gmail.com>
2023-11-17 21:13:23 -0500

    updating db connection

--- db.js
+++ db.js
@@ -7,23 +7,17 @@ let db;
 
 if (process.env.NODE_ENV === "production") {
   db = new Client({
-    host: "/var/run/postgresql/",
-    database: getDatabaseUri(),
+    connectionString: getDatabaseUri(),
     ssl: {
       rejectUnauthorized: false
     }
   });
 } else {
   db = new Client({
-    host: "/var/run/postgresql/",
-    database: getDatabaseUri()
+    connectionString: getDatabaseUri()
   });
 }
 
-const connectToDatabase = async () => {
-  await db.connect();
-}
-
-connectToDatabase()
+db.connect();
 
 module.exports = db;
